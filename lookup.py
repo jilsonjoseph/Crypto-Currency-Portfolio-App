23a4eef8681462216117264a106c2421e4b60eea
Jilson <4jilson@gmail.com>
2018-03-09 01:09:13 +0530

    First commit, Prints basic information of a set of crypto currencies

---
+++ lookup.py
@@ -0,0 +1,20 @@
+import requests
+import json
+import os
+os.system('cls')
+#################################
+
+
+api_request = requests.get("https://api.coinmarketcap.com/v1/ticker/")
+api = json.loads(api_request.content.decode('utf-8'))
+
+currencies = ["BTC", "XRP", "EOS", "STEEM"]
+
+print("---------------------")
+for x in api:
+	for coin in currencies:
+		if coin == x["symbol"]:
+			print(x["name"])
+			print("${0:.4f}".format(float(x["price_usd"])))
+			print("Rank {0:.0f}".format(float(x["rank"])))
+			print("---------------------")
